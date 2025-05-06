c725759ca46aa5c6c7301859e46349374cba679b
rianeksr19 <r.isid.545154@umindanao.edu.ph>
2025-05-07 00:23:03 +0800

    Admin

--- Consultation.Domain/Admin.cs
+++ Consultation.Domain/Admin.cs
@@ -10,7 +10,7 @@ namespace Consultation.Domain
     public class Admin
     {
         [Key]
-        public string UserName {  get; set; }
-        public string Password { get; set; }    
+        public string AdminID {  get; set; }
+       
     }
 }
