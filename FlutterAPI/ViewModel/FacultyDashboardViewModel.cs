b3af3b88ecb17c35061bb55b4d9e5bbe15784b10
Harwyne <h.basarte.550409@umindanao.edu.ph>
2025-05-14 15:11:19 +0800

    added FacultyDashboardViewModel

---
+++ FlutterAPI/ViewModel/FacultyDashboardViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Consultation.Domain;
+
+namespace FlutterAPI.ViewModel
+{
+    public class FacultyDashboardViewModel
+    {
+        public Student Student { get; set; }
+
+        public EnrolledCourse EnrolledCourse { get; set; }
+
+        public string ConsultationSchedule { get; set; }
+
+        public ConsultationRequest ConsultationRequest { get; set; }
+
+    }
+}
